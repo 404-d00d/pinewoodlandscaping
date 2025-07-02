e7c6812733abeae2a916bf1163e785071d25065f
404-d00d <raddivant@gmail.com>
2025-07-01 22:30:23 -0400

    Added images and image carosuel

--- scripting.js
+++ scripting.js
@@ -13,3 +13,32 @@ window.addEventListener('resize', () => {
       menuToggle.checked = false;
     }
 });
+
+  const images = [
+    "IMG_0167.jpg",
+    "IMG_0168.jpg",
+    "IMG_0169.jpg",
+    "IMG_0170.jpg",
+    "IMG_0171.jpg",
+    "IMG_0172.jpg",
+    "IMG_0173.jpg"
+  ];
+
+  let index = 0;
+  const lawnImage = document.getElementById("lawn-image");
+  const nextButton = document.getElementById("next");
+  const prevButton = document.getElementById("prev");
+
+  function updateImage() {
+    lawnImage.src = images[index];
+  }
+
+  nextButton.addEventListener("click", () => {
+    index = (index + 1) % images.length;
+    updateImage();
+  });
+
+  prevButton.addEventListener("click", () => {
+    index = (index - 1 + images.length) % images.length;
+    updateImage();
+  });
